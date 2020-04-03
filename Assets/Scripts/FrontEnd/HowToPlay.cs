b2dcd3a99adb3461f6bb10f05a4331c26976b6e8
BlazeDrake <55856582+BlazeDrake@users.noreply.github.com>
2020-04-03 11:14:56 -0600

    Added how to play button again
    I made it so that once you see the how to play screen once, you can see it again from the menu. I also made it so the continue button takes you back to the menu if you've seen it before

---
+++ Assets/Scripts/FrontEnd/HowToPlay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HowToPlay : MonoBehaviour
+{
+    public Button game;
+    public Button menu;
+    PlayerStats stats;
+    // Start is called before the first frame update
+    void Start()
+    {
+        stats = (PlayerStats)FindObjectOfType(typeof(PlayerStats));
+        if (stats.introSeen)
+        {
+            menu.gameObject.SetActive(true);
+            game.gameObject.SetActive(false);
+        }
+        stats.introSeen = true;
+    }
+
+}
--- Assets/Scripts/FrontEnd/MainMenu.cs
+++ Assets/Scripts/FrontEnd/MainMenu.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public PlayerStats stats;
+    public Button howToPlayButton;
     // Start is called before the first frame update
     void Start()
     {
         stats = (PlayerStats)FindObjectOfType(typeof(PlayerStats));
+        if (stats.introSeen)
+        {
+            howToPlayButton.gameObject.SetActive(true);
+        }
     }
 
     // Update is called once per frame
@@ -27,7 +33,6 @@ public class MainMenu : MonoBehaviour
         else
         {
             SceneLoad("HowToPlay");
-            stats.introSeen = true;
         }
     }
     public void SceneLoad(string name)
