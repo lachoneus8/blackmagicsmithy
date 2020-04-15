54279628c42d227f15ad3b3a3d2c3f32abeea885
BlazeDrake <55856582+BlazeDrake@users.noreply.github.com>
2020-04-15 08:59:34 -0600

    added playerprefs

--- Assets/Scripts/Gameplay/PlayerStats.cs
+++ Assets/Scripts/Gameplay/PlayerStats.cs
@@ -6,18 +6,55 @@ using UnityEngine;
 public class PlayerStats : MonoBehaviour
 {
     public bool introSeen;
+    public int highScore;
+    public List<string> keyNames;
     // Start is called before the first frame update
     void Start()
     {
+        if (PlayerPrefs.GetInt(keyNames[(int)keys.gameOpened], 0) == 0)
+        {
+            PlayerPrefs.SetInt(keyNames[(int)keys.gameOpened], 1);
+            PlayerPrefs.SetInt(keyNames[(int)keys.introSeen], 0);
+            PlayerPrefs.SetInt(keyNames[(int)keys.highScore], 0);
+        }
+        else
+        {
+            highScore= PlayerPrefs.GetInt(keyNames[(int)keys.highScore], 0);
+
+            int test = PlayerPrefs.GetInt(keyNames[(int)keys.introSeen], 0);
+            if (test == 0)
+            {
+                introSeen = false;
+            }
+            else
+            {
+                introSeen = true;
+            }
+        }
+
         if (SceneManager.sceneCount==1)
         {
             SceneManager.LoadScene("Startup", LoadSceneMode.Additive);
         }
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnApplicationQuit()
     {
-        
+        PlayerPrefs.SetInt(keyNames[(int)keys.highScore], highScore);
+        if (introSeen)
+        {
+            PlayerPrefs.SetInt(keyNames[(int)keys.introSeen], 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt(keyNames[(int)keys.introSeen], 0);
+        }
+        PlayerPrefs.Save();
+    }
+    //Use with the KeyNames list
+    enum keys
+    {
+        gameOpened,
+        highScore,
+        introSeen
     }
 }
